9b2ce29b9a820e0128b927b20fed2dc6991674d9
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-08-14 14:13:22 +0530

    3 Questions

--- src/DecimalToBinary.java
+++ src/DecimalToBinary.java
@@ -1,7 +1,19 @@
 public class DecimalToBinary
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        function(12);
 
+    }
+
+    static void function(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        function(n/2);
+        System.out.print(n%2);
     }
 
 }
--- src/RecursionPractice/TwoStepRecursion.java
+++ src/RecursionPractice/TwoStepRecursion.java
@@ -4,7 +4,9 @@ public class TwoStepRecursion
 {
     public static void main(String[] args)
     {
-        fun(3);
+//        fun(3);
+//        System.out.println(fun1(64));
+        fun2(7);
 
     }
     static void fun(int n)
@@ -16,5 +18,27 @@ public class TwoStepRecursion
         fun(n-1);
         System.out.println(n);
         fun(n-1);
+    }
+
+    static int fun1(int n)
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1 + fun1(n/2);
+        }
+    }
+
+    static void fun2(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        fun2(n/2);
+        System.out.println(n%2);
     }
 }
