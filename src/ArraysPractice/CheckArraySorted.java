757aa390551e36118afe816a0bd4a9f181a03abe
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-09-05 18:56:20 +0530

    Pata nahi kya hua

--- src/ArraysPractice/CheckArraySorted.java
+++ src/ArraysPractice/CheckArraySorted.java
@@ -1,2 +1,41 @@
-package ArraysPractice;public class CheckArraySorted {
+package ArraysPractice;
+
+public class CheckArraySorted
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {1,2,3};
+        System.out.println(checkSorted(arr));
+        System.out.println(checkSort(arr));
+
+    }
+    static boolean checkSorted(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = i+1; j < arr.length ; j++)
+            {
+                if (arr[j]<arr[i])
+                {
+                    return false;
+                }
+
+            }
+
+        }
+        return true;
+    }
+
+    static boolean checkSort(int arr[])
+    {
+        for (int i = 1; i < arr.length; i++)
+        {
+            if (arr[i]<arr[i-1])
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
 }
--- src/ArraysPractice/ReverseArray.java
+++ src/ArraysPractice/ReverseArray.java
@@ -1,2 +1,29 @@
-package ArraysPractice;public class ReverseArray {
+package ArraysPractice;
+
+public class ReverseArray
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {4,5,6,7};
+        reverse(arr);
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+
+        }
+
+    }
+
+    static void reverse(int arr[])
+    {
+        int low = 0 , high = arr.length-1;
+        while (low<high)
+        {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
 }
