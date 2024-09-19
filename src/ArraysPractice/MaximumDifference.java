f1cf0bce930d0df75b95d23de47ef6ed1ac0f251
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-09-19 18:31:40 +0530

    Little tweaks

--- src/ArraysPractice/MaximumDifference.java
+++ src/ArraysPractice/MaximumDifference.java
@@ -1,12 +1,15 @@
 package ArraysPractice;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MaximumDifference
 {
     public static void main(String[] args)
     {
-        int arr[] = {2, 3, 10, 6, 4, 8, 1};
+        int arr[] = {20, 3, 10, 6, 4, 8, 1};
         System.out.println(maxDifference(arr));
-
+        System.out.println(maximumDifference(arr));
     }
 
     static int maxDifference(int arr[])
@@ -21,6 +24,18 @@ public class MaximumDifference
                 result = Math.max(result,arr[j]-arr[i]);
             }
             
+        }
+        return result;
+    }
+
+    static int maximumDifference(int arr[])
+    {
+        int result = arr[1]-arr[0];
+        int minValue=arr[0];
+        for (int i = 1; i <arr.length ; i++)
+        {
+            result = Math.max(result,arr[i]-minValue);
+            minValue=Math.min(minValue,arr[i]);
         }
         return result;
     }
