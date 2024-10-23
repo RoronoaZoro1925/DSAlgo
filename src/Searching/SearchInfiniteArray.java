bdd00c01d49dcb4fc46d678d1b8838506e508926
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-10-23 13:57:13 +0530

    Sorted Rotated Array

--- src/Searching/SearchInfiniteArray.java
+++ src/Searching/SearchInfiniteArray.java
@@ -2,11 +2,41 @@ package Searching;
 
 public class SearchInfiniteArray
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int arr [] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        int x = 12;
+        System.out.println(searchInfinite(arr,x));
 
     }
-    public static int searchInfinite()
+    public static int searchInfinite(int arr[], int x)
     {
-        return -1; 
+        if(arr[0] == x)
+            return -1;
+        int i = 1;
+        while (arr[i]<x)
+        {
+            i = i*2;
+        }
+        if (arr[i]==x)
+            return i;
+        return binarySearch(arr,x,i/2+1,i-1);
+    }
+    public static int binarySearch(int arr[], int x, int low, int high)
+    {
+        if (low>high)
+            return -1;
+        int mid = (low+high)/2;
+        if(arr[mid] == x)
+            return mid;
+        else if (arr[mid]>x)
+        {
+            return binarySearch(arr, x, low , mid-1);
+        }
+        else
+        {
+            return binarySearch(arr, x, mid+1 , high);
+        }
+
     }
 }
---
+++ src/Searching/SortedRotatedArray.java
@@ -0,0 +1,42 @@
+package Searching;
+
+public class SortedRotatedArray
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {10,20,40,60,5,8};
+        int x = 5;
+        System.out.println(sortrotatedSearch(arr,x));
+
+    }
+
+    public static int sortrotatedSearch(int arr[], int x)
+    {
+        int low = 0 , high = arr.length-1;
+        while (low<=high)
+        {
+            int mid = (low+high)/2;
+            if (arr[mid] == x)
+                return mid;
+            if(arr[low]<=arr[mid])
+            {
+                if (x>=arr[low]&&x<arr[mid])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else
+            {
+                if (x>arr[mid]&&x<=arr[high])
+                {
+                    low=mid+1;
+                }
+                else
+                    high=mid-1;
+
+            }
+        }
+        return -1;
+
+    }
+}
